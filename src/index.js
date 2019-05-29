283e4e57e28e68037e69fc65f69d1924dcd870d5
Ketino <ketino_gr@ukr.net>
2019-05-29 15:16:51 +0300

    add project API

---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+class SwapiService {
+
+  _apiBase = 'https://swapi.co/api' ;
+
+  async getResource(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+
+    if(!res.ok) {
+      throw new Error(`Could not fetch ${url}` +  `, received ${res.status}`)
+    }
+    const body = await res.json();
+    return body;
+  }
+
+  getAllPeople() {
+    return this.getResource(`/people/`);
+  }
+
+  getPerson(id) {
+    return this.getResource(`/people/${id}`);
+  }
+}
+
+const swapi = new SwapiService();
+
+swapi.getAllPeople().then((body) => {
+  console.log(body);
+})
